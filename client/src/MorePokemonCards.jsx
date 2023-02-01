1162cd4de21458efd41c55bcd13256b44045cdd2
Kan5661 <linkan0718@gmail.com>
2023-02-01 12:00:24 -0500

    search bar

--- client/src/MorePokemonCards.jsx
+++ client/src/MorePokemonCards.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react'
 
 function SearchPokemon() {
-    const [allPokemon, setAllPokemon] = useState({})
+    const [allPokemon, setAllPokemon] = useState([])
+    const [searchValue, setSearchValue] = useState('')
+    const [searchTotal, setSearchTotal] = useState(10)
+    const pokemonList = []
+    function handleChange(e) {
+        console.log(e.target.value)
+    }
 
     function getScroll() {
         let scrollPosition = window.innerHeight + window.scrollY
@@ -10,11 +16,30 @@ function SearchPokemon() {
             console.log('you are at the bottom of the page')
         }
     }
+
+    function fetchPokemon() {
+        if (searchValue === '') {
+            for (let i = 0; i <= searchTotal; i++) {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${searchTotal}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const newPokemon = {
+                        name: data.name
+                    }
+                    pokemonList.push(newPokemon)
+                    setAllPokemon(pokemonList)
+                })
+            }
+        }
+    }
     window.onscroll = () => getScroll()
+
+    useEffect(() => fetchPokemon(), [])
 
     return(
         <div className='allCardsPage'>
-            new branch
+            <input className='searchInput' onChange={handleChange}></input>
         </div>
     )
 }
